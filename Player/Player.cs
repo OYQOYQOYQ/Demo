928521708174df575918e737bcff57c34919d541
OYQ <3195759332@qq.com>
2026-04-17 00:45:56 +0800

    feat(Player): 添加基础玩家移动和动画控制逻辑
    实现玩家角色的基础移动功能，包括方向输入检测、速度控制和动画状态切换

---
+++ Player/Player.cs
@@ -0,0 +1,38 @@
+using Godot;
+
+namespace Demo.Player;
+
+public partial class Player : CharacterBody2D
+{
+	[Export]
+	private AnimationTree _animationTree;
+	[Export]
+	private float MoveSpeed {get; set;} = 100.0f;
+
+	private Vector2 _direction;
+	private Vector2 _lastDirection = Vector2.Down;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		_direction = Input.GetVector("LeftMove", "RightMove", "ForwardMove", "BackMove");
+		if (_direction != Vector2.Zero)
+		{
+			if (_direction != _lastDirection)
+			{
+				_lastDirection = _direction;
+			}
+			_animationTree.Set("parameters/conditions/IsIdle", false);
+			_animationTree.Set("parameters/conditions/IsRunning", true);
+			_animationTree.Set("parameters/PlayerRun/blend_position", _direction);
+			Velocity = _direction * MoveSpeed;
+		}
+		else
+		{
+			Velocity = Vector2.Zero;
+			_animationTree.Set("parameters/conditions/IsIdle", true);
+			_animationTree.Set("parameters/conditions/IsRunning", false);
+			_animationTree.Set("parameters/PlayerIdle/blend_position", _lastDirection);
+		}
+		MoveAndSlide();
+	}
+}
