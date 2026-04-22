5b82f23fa466760685904c27a9f75fdd1baa9d62
OYQ <3195759332@qq.com>
2026-04-23 05:18:57 +0800

    feat(玩家): 重构玩家行为系统并优化树木交互
    - 重构玩家行为状态机，使用更清晰的枚举和状态管理
    - 优化树木交互逻辑，增加透明度和碰撞检测
    - 调整玩家移动速度和动画参数
    - 新增多棵树实例并调整位置
    - 更新项目配置和图层命名

--- Core/Player/Player.cs
+++ Core/Player/Player.cs
@@ -1,159 +1,142 @@
 using Godot;
 
-namespace Demo.Player;
+namespace Demo.Core.Player;
 
-public enum EBehaviorState
+public enum BehaviorState
 {
-	Null,
-	Hoeing,
-	ChopTree,
-	Water,
+    Null,
+    Hoeing,
+    ChopTree,
+    Water,
 }
 
 public partial class Player : CharacterBody2D
 {
-	[Export]
-	private Area2D _area;
-	[Export]
-	private AnimationTree _animationTree;
-	[Export]
-	private float MoveSpeed {get; set;} = 100.0f;
-	[Export]
-	private Sprite2D _sprite;
-	[Export]
-	private EBehaviorState _behaviorState;
-
+    [ExportGroup("Attribute")]
+    // 玩家移动属性
+    [Export] private float _moveSpeed = 80.0f;
     private Vector2 _direction;
-	private Vector2 _lastDirection = Vector2.Down;
-	private Vector2 _currentBehaviorDirection;
-	private AnimationNodeStateMachinePlayback _stateMachinePlayback;
-	private StringName _currentBehaviorState;
-    private bool _enterTree;
+    private Vector2 _lastDirection = Vector2.Down;
+    // 玩家行为属性
+    [Export] private BehaviorState _currentBehaviorState;
+    private StringName _behaviorState;
+    private Vector2 _currentBehaviorDirection;
+
+    [ExportGroup("NodeInitialization")]
+    // 玩家动画属性
+    [Export] private Sprite2D _moveSprite;
+    [Export] private Area2D _occlusionDetecion;
+    [Export] private AnimationTree _animationTree;
+    private AnimationNodeStateMachinePlayback _stateMachinePlayback;
+
     private Node2D _obj;
 
     public override void _Ready()
-	{
-		_stateMachinePlayback = _animationTree.Get("parameters/playback").As<AnimationNodeStateMachinePlayback>();
-		_animationTree.AnimationFinished += OnAnimationFinished;
-		_area.BodyEntered += OnBodyEntered;
-		_area.BodyExited += OnBodyExited;
-	}
+    {
+        _stateMachinePlayback = _animationTree.Get("parameters/playback").As<AnimationNodeStateMachinePlayback>();
+        _animationTree.AnimationFinished += OnAnimationFinished;
+        _occlusionDetecion.BodyEntered += OnBodyEntered;
+        _occlusionDetecion.BodyExited += OnBodyExited;
+    }
 
-	public override void _PhysicsProcess(double delta)
-	{
-		CurrentBehaviorState(_behaviorState);
+    public override void _PhysicsProcess(double delta)
+    {
+        _behaviorState = GetCurrentBehaviorState(_currentBehaviorState);
 
-		bool isCurrentlyHoeing = _animationTree.Get($"parameters/conditions/Is{_currentBehaviorState}").AsBool();
+        bool isCurrentlyHoeing = _animationTree.Get($"parameters/conditions/Is{_currentBehaviorState}").AsBool();
 
-        if (!isCurrentlyHoeing && _sprite.Visible)
-		{
-			_direction = Input.GetVector("LeftMove", "RightMove", "ForwardMove", "BackMove");
-			if (_direction != Vector2.Zero)
-			{
-				if (_direction != _lastDirection)
-				{
-					_lastDirection = _direction;
-				}
-				_animationTree.Set("parameters/conditions/IsIdle", false);
-				_animationTree.Set("parameters/conditions/IsRunning", true);
-				_animationTree.Set("parameters/PlayerRun/blend_position", _direction);
-				Velocity = _direction * MoveSpeed;
-			}
-			else
-			{
-				Velocity = Vector2.Zero;
-				_animationTree.Set("parameters/conditions/IsIdle", true);
-				_animationTree.Set("parameters/conditions/IsRunning", false);
-				_animationTree.Set("parameters/PlayerIdle/blend_position", _lastDirection);
-			}
-			MoveAndSlide();
-		}
-
-		bool isHoeing = Input.IsActionJustPressed("Hoeing");
-		if (isHoeing && !isCurrentlyHoeing)
-		{
-            _animationTree.Set("parameters/conditions/IsIdle", false);
-			_animationTree.Set("parameters/conditions/IsRunning", false);
-			_animationTree.Set($"parameters/conditions/Is{_currentBehaviorState}", true);
-
-			if (_direction != Vector2.Zero)
-			{
-				_currentBehaviorDirection = _direction;
-			}
-			else
-			{
-				_currentBehaviorDirection = _lastDirection;
-			}
-			_animationTree.Set($"parameters/{_currentBehaviorState}/blend_position", _currentBehaviorDirection);
+        if (!isCurrentlyHoeing && _moveSprite.Visible)
+        {
+            _direction = Input.GetVector("LeftMove", "RightMove", "ForwardMove", "BackMove");
+            if (_direction != Vector2.Zero)
+            {
+                if (_direction != _lastDirection)
+                {
+                    _lastDirection = _direction;
+                }
+                _animationTree.Set("parameters/conditions/IsIdle", false);
+                _animationTree.Set("parameters/conditions/IsRunning", true);
+                _animationTree.Set("parameters/PlayerRun/blend_position", _direction);
+                Velocity = _direction * _moveSpeed;
+            }
+            else
+            {
+                Velocity = Vector2.Zero;
+                _animationTree.Set("parameters/conditions/IsIdle", true);
+                _animationTree.Set("parameters/conditions/IsRunning", false);
+                _animationTree.Set("parameters/PlayerIdle/blend_position", _lastDirection);
+            }
+            MoveAndSlide();
         }
-	}
 
-	public override void _ExitTree()
-	{
-		_animationTree.AnimationFinished -= OnAnimationFinished;
-		_area.BodyEntered -= OnBodyEntered;
-		_area.BodyExited -= OnBodyExited;
-	}
+        bool isHoeing = Input.IsActionJustPressed("Hoeing");
+        if (isHoeing && !isCurrentlyHoeing)
+        {
+            _animationTree.Set("parameters/conditions/IsIdle", false);
+            _animationTree.Set("parameters/conditions/IsRunning", false);
+            _animationTree.Set($"parameters/conditions/Is{_currentBehaviorState}", true);
 
-	private void OnAnimationFinished(StringName animationName)
-	{
-		if (animationName.ToString().Contains(_currentBehaviorState))
-		{
+            if (_direction != Vector2.Zero)
+            {
+                _currentBehaviorDirection = _direction;
+            }
+            else
+            {
+                _currentBehaviorDirection = _lastDirection;
+            }
+            _animationTree.Set($"parameters/{_currentBehaviorState}/blend_position", _currentBehaviorDirection);
+        }
+    }
+
+    public override void _ExitTree()
+    {
+        _animationTree.AnimationFinished -= OnAnimationFinished;
+        _occlusionDetecion.BodyEntered -= OnBodyEntered;
+        _occlusionDetecion.BodyExited -= OnBodyExited;
+    }
+
+    private void OnAnimationFinished(StringName animationName)
+    {
+        if (animationName.ToString().Contains(_behaviorState))
+        {
             _animationTree.Set($"parameters/conditions/Is{_currentBehaviorState}", false);
             _animationTree.Set("parameters/PlayerIdle/blend_position", _lastDirection);
             _stateMachinePlayback.Travel("PlayerIdle");
-
-            if (_enterTree)
-            {
-                int hp = (int)_obj.GetMeta("HealthPoints");
-                hp--;
-                if (hp <= 0)
-                {
-                    _obj.QueueFree();
-                }
-                else
-                {
-                    _obj.SetMeta("HealthPoints", hp);
-                }
-            }
         }
-	}
-
-	private void OnBodyEntered(Node2D body) 
-	{
-        if (body.GetParent().Name == "Tree") 
-		{
-			_obj = body.GetParent() as Node2D;
-            _obj.Modulate = new Color(1, 1, 1, 0.5f);
-			_enterTree = true;
-        }
-	}
-
-    private void OnBodyExited(Node2D body) 
-	{
-        if (body.GetParent().Name == "Tree")
-        { 
-			_obj = body.GetParent() as Node2D;
-            _obj.Modulate = new Color(1, 1, 1, 1);
-			_enterTree = false;
-		}
     }
 
-    private void CurrentBehaviorState(EBehaviorState behaviorState)
-	{
-		switch (behaviorState)
-		{
-			case EBehaviorState.Null:
-				break;
-			case EBehaviorState.Hoeing:
-				_currentBehaviorState = "Hoeing";
-				break;
-			case EBehaviorState.ChopTree:
-				_currentBehaviorState = "ChopTree";
-				break;
-			case EBehaviorState.Water:
-				_currentBehaviorState = "Water";
-				break;
-		}
-	}
+    private void OnBodyEntered(Node2D body)
+    {
+        if ((bool)body.GetParent().GetMeta("Transparent"))
+        {
+            _obj = body.GetParent() as Node2D;
+            _obj.Modulate = new Color(1, 1, 1, 0.5f);
+        }
+    }
+
+    private void OnBodyExited(Node2D body)
+    {
+        if ((bool)body.GetParent().GetMeta("Transparent"))
+        {
+            _obj = body.GetParent() as Node2D;
+            _obj.Modulate = new Color(1, 1, 1, 1);
+        }
+    }
+
+    private StringName GetCurrentBehaviorState(BehaviorState behaviorState)
+    {
+        switch (behaviorState)
+        {
+            case BehaviorState.Null:
+                return "";
+            case BehaviorState.Hoeing:
+                return "Hoeing";
+            case BehaviorState.ChopTree:
+                return "ChopTree";
+            case BehaviorState.Water:
+                return "Water";
+            default:
+                return "";
+        }
+    }
 }
